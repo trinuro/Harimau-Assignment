b24e29a6aabe998d07a1d21f00ae5a772e77e0d7
trinuro <66290935+trinuro@users.noreply.github.com>
2023-12-09 08:59:17 +0800

    Add makeNewDonation and getDonationsData method

--- LogIn_SignUp/src/main/java/Utilities.java
+++ LogIn_SignUp/src/main/java/Utilities.java
@@ -56,7 +56,7 @@ public class Utilities {
         return output;            
     }
     
-    public static boolean buyMerch(String username, String merchName, String numberOfMerch, String deliveryAddress){
+    public static boolean buyMerch(String username, String merchName, int numberOfMerch, String deliveryAddress){
         String userID = login.getUserData(username, "user_id");
         if(userID.equals("")){
             // Returns false if user does not exists
@@ -69,7 +69,7 @@ public class Utilities {
         ){
 
         // SQL command to be executed
-        String sqlInsert = String.format("INSERT INTO MerchandiseOrder(buyer_id,merchandise_id,purchase_amount,delivery_address) VALUES(%s,\"%s\",\"%s\",\"%s\");", userID, merchName, numberOfMerch, deliveryAddress);
+        String sqlInsert = String.format("INSERT INTO MerchandiseOrder(buyer_id,merchandise_id,purchase_amount,delivery_address) VALUES(%s,\"%s\",\"%d\",\"%s\");", userID, merchName, numberOfMerch, deliveryAddress);
         System.out.println("SQL Statement to be executed: "+sqlInsert);
         // Insert information into database
         int countInserted = stmt.executeUpdate(sqlInsert);
@@ -142,6 +142,73 @@ public class Utilities {
 
         // SQL command to be executed
         String sqlInsert = String.format("INSERT INTO trees(buyer_id, tree_name) VALUES(%s,\"%s\");", userID, treeName);
+        System.out.println("SQL Statement to be executed: "+sqlInsert);
+        // Insert information into database
+        int countInserted = stmt.executeUpdate(sqlInsert);
+        System.out.println(countInserted+" records inserted.");
+            
+        }catch(SQLException ex){
+            System.out.println("SQL failed! Find Khiew");
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    public static ArrayList<String> getDonationsData(String username){
+        // This methods retrieve all donations a user has made given his username
+        
+        String outputString;
+        ArrayList<String> output= new ArrayList<String>();
+        
+        // Connect to database
+        try(
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz_data", "root", "harimau");
+            Statement stmt = conn.createStatement();
+            ){
+        // Create SQL query
+        String strSelect = String.format("""
+            SELECT user_table.username, donations_history.donation_amount, donations_history.organisation
+            FROM user_table
+            JOIN donations_history
+            ON donations_history.donor_id = user_table.user_id
+            WHERE username = "%s";                        
+                                         """, username);
+        System.out.println("The SQL statement is "+strSelect);
+
+        // Execute query
+        ResultSet rset = stmt.executeQuery(strSelect);
+
+        int rowCount = 0;
+        // Get purchase history of a user from database
+        while(rset.next()){
+            outputString = String.format("%s has donated %s to %s", rset.getString("username"), rset.getString("donation_amount"),rset.getString("organisation"));
+            output.add(outputString);
+            rowCount++;
+        } 
+        }catch(SQLException ex){
+            System.out.println("SQL query failed.");
+            ex.printStackTrace();       
+        }    
+
+        return output;  
+    }
+    
+    public static boolean makeNewDonations(String username, int donationAmount, String organisationName){
+        // Get user ID given username
+        String userID = login.getUserData(username, "user_id");
+        // Check whether user exists
+        if(userID.equals("")){
+            return false;
+        }
+        try(
+//          Create connection to database
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz_data", "root", "harimau");
+            Statement stmt = conn.createStatement();
+        ){
+
+        // SQL command to be executed
+        String sqlInsert = String.format("INSERT INTO donations_history(donor_id,donation_amount,organisation) VALUES(%s,%d,\"%s\");", userID, donationAmount,organisationName);
         System.out.println("SQL Statement to be executed: "+sqlInsert);
         // Insert information into database
         int countInserted = stmt.executeUpdate(sqlInsert);
