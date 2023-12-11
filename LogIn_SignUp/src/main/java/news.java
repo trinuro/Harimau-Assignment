29536ba9f8acf50667b1c9db0b280fe13f3b7762
Lxying0114 <147296607+Lxying0114@users.noreply.github.com>
2023-12-11 16:50:54 +0800

    Remove unwanted files

---
+++ LogIn_SignUp/src/main/java/news.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DatabaseMetaData;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class news implements Comparable<news>{
+    private String specificDate;
+    private String specificTitle;
+    private String specificURL;
+    private Date holdDate;
+    private static ArrayList<news> newsArrayList = new ArrayList<>();
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+    
+    public news(String specifictTitle, String specificURL, String specificDate) {
+        this.specificTitle = specifictTitle;
+        this.specificURL = specificURL;
+        this.specificDate = specificDate;
+        try {
+            this.holdDate = simpleDateFormat.parse(specificDate);
+        } catch (Exception e) {
+            System.out.println("Invalid date");
+        }
+        
+    }
+    
+    @Override
+    public int compareTo(news news){
+        return getSpecificHoldDate().compareTo(news.getSpecificHoldDate());
+    }
+    
+    // get method for each data field
+    public Date getSpecificHoldDate () {
+        return holdDate;
+    }
+    public String returnSpecificDate () {
+        return specificDate;
+    }
+    public String returnSpecificTitle () {
+        return specificTitle;
+    }
+    public String returnSpecificURL () {
+        return specificURL;
+    }
+    //WHAT CAN BE USED
+    public static String getSpecificDate (int noOFNews) {
+        return newsArrayList.get(noOFNews).returnSpecificDate();
+    }
+    public static String getSpecificTitle (int noOFNews) {
+        return newsArrayList.get(noOFNews).returnSpecificTitle();
+    }
+    public static String getSpecificURL (int noOFNews) {
+        return newsArrayList.get(noOFNews).returnSpecificURL();
+    }
+    //UNTIL HERE (JUST THESE 3 METHOD)
+    
+    // det method for each data field except holdDate as holdDate is depending the specificDate
+    public void setSpecificDate (String specificDate) {
+       this.specificDate = specificDate;
+    }
+    public void setSpecificTitle (String specificTitle) {
+       this.specificTitle = specificTitle;
+    }
+    public void setSpecificURL (String specificURL) {
+        try {
+            this.holdDate = simpleDateFormat.parse(specificDate);
+        } catch (Exception e) {
+            System.out.println("Invalid date");
+        }
+       this.specificURL = specificURL;
+    }
+    
+    public static void main(String[] args){
+        // declare filepath
+        System.out.println("curretn : " + System.getProperty("user.dir"));
+//        String filepath = "C:\\Users\\PC\\Documents\\NetBeansProjects\\harimau_newsTrial\\src\\main\\java\\com\\mycompany\\harimau_newstrial\\NewsSample.txt";
+        String filepath = "NewSample.txt";
+        
+        // convert the file into arrrayList
+        ArrayList<String> l1 = readFileInArrayList(filepath);
+        
+        // create arraylist for each title, date, url
+        ArrayList<String> titleList = getArrayList(l1, 3);
+        ArrayList<String> urlList = getArrayList(l1, 1);
+        ArrayList<String> dateList = getArrayList(l1, 2);
+
+        
+        for (int i = 0; i < titleList.size(); i++) {
+            String[] title = titleList.get(i).split(" ");
+            boolean hasNatureKeyword = false;
+            for (int j = 0; j < title.length; j++) {
+                hasNatureKeyword = false;
+                if (title[j].equalsIgnoreCase("nature")) {
+                    hasNatureKeyword = true; break;
+                }
+            }
+            if (!hasNatureKeyword) {
+                titleList.remove(titleList.get(i));
+                urlList.remove(urlList.get(i));
+                dateList.remove(dateList.get(i));
+                i--;
+            }
+        }
+        
+        for (int i = 0; i < titleList.size(); i++) {
+            news news = new news(titleList.get(i), urlList.get(i), dateList.get(i));
+            newsArrayList.add(news);
+        }
+        
+        // sort the ArrayList of News according to their specificDate
+        Collections.sort(newsArrayList, Collections.reverseOrder());
+        
+        //SEE THE DEMO HERE
+        for (int i = 0; i < newsArrayList.size(); i++) {
+            System.out.println(getSpecificTitle(i));
+            System.out.println(getSpecificURL(i));
+            System.out.println(getSpecificDate(i));
+        }
+        
+        
+    }
+    
+    
+    // create a method to read file as list
+    public static ArrayList<String> readFileInArrayList(String filepath) {
+        List<String> fileList = new ArrayList<>();
+        try {
+            fileList = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (ArrayList<String>)fileList;
+    }
+    
+    
+    // create a method to create titleList/urlList/dateList
+    public static ArrayList<String> getArrayList(ArrayList<String> originalArrayList, int multipleOfLine) {
+        ArrayList<String> newArray = new ArrayList<>();
+        for (int i = 0; i < originalArrayList.size(); i++) {
+            if ((i-multipleOfLine)%3 == 0) {
+                newArray.add(originalArrayList.get(i));
+            }
+        }
+        return newArray;
+    }
+    
+//    public static String getData(String username, String columnTitle){
+//        // This function returns data from database based on username and column title
+//        String output="";
+//        // Connect to database
+//        try(
+//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz_data", "root", "harimau");
+//            Statement stmt = conn.createStatement();
+//            ){
+//        // Create SQL query
+//        String strSelect = String.format("SELECT %s FROM user_table WHERE username = \'%s\';",columnTitle,username);
+//        System.out.println("The SQL statement is "+strSelect);
+//        
+//        // Execute query
+//        ResultSet rset = stmt.executeQuery(strSelect);
+//
+//        int rowCount = 0;
+//        // Get last_checked_in date from database
+//        while(rset.next()){
+//            output = rset.getString(columnTitle);
+//            rowCount++;
+//        } 
+//        }catch(SQLException ex){
+//            System.out.println("SQL query failed.");
+//            ex.printStackTrace();
+//        }    
+//        return output;
+//    }
+    
+   
+    
+
+    
+}
+
