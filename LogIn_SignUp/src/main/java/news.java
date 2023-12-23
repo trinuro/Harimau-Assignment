78bc8ab82bed692e4c4717f93cd8948715f9751f
Lxying0114 <147296607+Lxying0114@users.noreply.github.com>
2023-12-23 16:44:27 +0800

    update news

--- LogIn_SignUp/src/main/java/news.java
+++ LogIn_SignUp/src/main/java/news.java
@@ -14,7 +14,7 @@ public class news implements Comparable<news>{
     private String specificTitle;
     private String specificURL;
     private Date holdDate;
-    private static ArrayList<news> newsArrayList = new ArrayList<>();
+    private static final ArrayList<news> newsArrayList = createNewsArrayList();
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
     
     public news(String specifictTitle, String specificURL, String specificDate) {
@@ -47,19 +47,8 @@ public class news implements Comparable<news>{
     public String returnSpecificURL () {
         return specificURL;
     }
-    //WHAT CAN BE USED
-    public static String getSpecificDate (int noOFNews) {
-        return newsArrayList.get(noOFNews).returnSpecificDate();
-    }
-    public static String getSpecificTitle (int noOFNews) {
-        return newsArrayList.get(noOFNews).returnSpecificTitle();
-    }
-    public static String getSpecificURL (int noOFNews) {
-        return newsArrayList.get(noOFNews).returnSpecificURL();
-    }
-    //UNTIL HERE (JUST THESE 3 METHOD)
     
-    // det method for each data field except holdDate as holdDate is depending the specificDate
+  // det method for each data field except holdDate as holdDate is depending the specificDate
     public void setSpecificDate (String specificDate) {
        this.specificDate = specificDate;
     }
@@ -73,80 +62,146 @@ public class news implements Comparable<news>{
             System.out.println("Invalid date");
         }
        this.specificURL = specificURL;
+    } 
+    
+    
+    //***********************************************************************
+    // *******************START FROM HERE**********************************
+    
+    public static String getSpecificDate (int noOFNews) {
+        return newsArrayList.get(noOFNews).returnSpecificDate();
+    }
+    public static String getSpecificTitle (int noOFNews) {
+        return newsArrayList.get(noOFNews).returnSpecificTitle();
+    }
+    public static String getSpecificURL (int noOFNews) {
+        return newsArrayList.get(noOFNews).returnSpecificURL();
     }
     
-    public static void main(String[] args){
-        // declare filepath
-        System.out.println("curretn : " + System.getProperty("user.dir"));
-//        String filepath = "C:\\Users\\PC\\Documents\\NetBeansProjects\\harimau_newsTrial\\src\\main\\java\\com\\mycompany\\harimau_newstrial\\NewsSample.txt";
-        String filepath = "NewSample.txt";
-        
-        // convert the file into arrrayList
-        ArrayList<String> l1 = readFileInArrayList(filepath);
-        
-        // create arraylist for each title, date, url
-        ArrayList<String> titleList = getArrayList(l1, 3);
-        ArrayList<String> urlList = getArrayList(l1, 1);
-        ArrayList<String> dateList = getArrayList(l1, 2);
+    // ***********************************Until here****************************************
+    //**************************************************************************************
+    
 
-        
-        for (int i = 0; i < titleList.size(); i++) {
-            String[] title = titleList.get(i).split(" ");
-            boolean hasNatureKeyword = false;
-            for (int j = 0; j < title.length; j++) {
-                hasNatureKeyword = false;
-                if (title[j].equalsIgnoreCase("nature")) {
-                    hasNatureKeyword = true; break;
-                }
-            }
-            if (!hasNatureKeyword) {
-                titleList.remove(titleList.get(i));
-                urlList.remove(urlList.get(i));
-                dateList.remove(dateList.get(i));
-                i--;
-            }
-        }
-        
-        for (int i = 0; i < titleList.size(); i++) {
-            news news = new news(titleList.get(i), urlList.get(i), dateList.get(i));
-            newsArrayList.add(news);
-        }
-        
-        // sort the ArrayList of News according to their specificDate
-        Collections.sort(newsArrayList, Collections.reverseOrder());
-        
+    
+    public static void main(String[] args) {
         //SEE THE DEMO HERE
         for (int i = 0; i < newsArrayList.size(); i++) {
             System.out.println(getSpecificTitle(i));
             System.out.println(getSpecificURL(i));
             System.out.println(getSpecificDate(i));
         }
-        
-        
     }
     
-    
-    // create a method to read file as list
-    public static ArrayList<String> readFileInArrayList(String filepath) {
-        List<String> fileList = new ArrayList<>();
-        try {
-            fileList = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return (ArrayList<String>)fileList;
-    }
-    
-    
-    // create a method to create titleList/urlList/dateList
-    public static ArrayList<String> getArrayList(ArrayList<String> originalArrayList, int multipleOfLine) {
-        ArrayList<String> newArray = new ArrayList<>();
-        for (int i = 0; i < originalArrayList.size(); i++) {
-            if ((i-multipleOfLine)%3 == 0) {
-                newArray.add(originalArrayList.get(i));
+    // by connecting to database
+    public static ArrayList<news> createNewsArrayList(){
+        // create arraylist that will be storing the news with only contain keyword "nature" in the tilte
+        ArrayList<news> newsArray = new ArrayList<>();
+        // create arraylist for each title, date, url
+        ArrayList<String> titleList = new ArrayList<>();
+        ArrayList<String> urlList = new ArrayList<>();
+        ArrayList<String> dateList = new ArrayList<>();
+
+        
+        for (int i = 1; i <= 10; i++) {
+            String[] title = Utilities.getNews(i)[0].split(" ");
+            boolean hasNatureKeyword = false;
+            
+            // check if the titie contains keyword "nature"
+            for (int j = 0; j < title.length; j++) {
+                hasNatureKeyword = false;
+                if (title[j].equalsIgnoreCase("nature")) {
+                    hasNatureKeyword = true; break;
+                }
+            }
+            
+            // add the news with keyword "nature" in the title
+            if (hasNatureKeyword) {
+                titleList.add(Utilities.getNews(i)[0]);
+                urlList.add(Utilities.getNews(i)[1]);
+                dateList.add(Utilities.getNews(i)[2]);
             }
         }
-        return newArray;
+        
+        // add the news with keyword "nature" in the titile into news arrayList
+        for (int i = 0; i < titleList.size(); i++) {
+            news news = new news(titleList.get(i), urlList.get(i), dateList.get(i));
+            newsArray.add(news);
+        }
+        
+        // sort the ArrayList of News according to their specificDate, from the latest to the oldest news
+        Collections.sort(newsArray, Collections.reverseOrder());
+        
+        return newsArray;
     }
-   
+    
+    
+//    //by using NewSample.txt
+//    public static ArrayList<news> createNewsArrayList(){
+//        ArrayList<news> newsArray = new ArrayList<>();
+//        // declare filepath
+//        String filepath = "src/main/resources/NewSample.txt";
+//        // convert the file into arrrayList
+//        ArrayList<String> l1 = readFileInArrayList(filepath);
+//        // create arraylist for each title, date, url
+//        ArrayList<String> titleList = getArrayList(l1, 3);
+//        ArrayList<String> urlList = getArrayList(l1, 1);
+//        ArrayList<String> dateList = getArrayList(l1, 2);
+//
+//        
+//        for (int i = 0; i < titleList.size(); i++) {
+//            String[] title = titleList.get(i).split(" ");
+//            boolean hasNatureKeyword = false;
+//            
+//            // check if the titie contains keyword "nature"
+//            for (int j = 0; j < title.length; j++) {
+//                hasNatureKeyword = false;
+//                if (title[j].equalsIgnoreCase("nature")) {
+//                    hasNatureKeyword = true; break;
+//                }
+//            }
+//            
+//            // remove the news without keyword "nature"
+//            if (!hasNatureKeyword) {
+//                titleList.remove(titleList.get(i));
+//                urlList.remove(urlList.get(i));
+//                dateList.remove(dateList.get(i));
+//                i--;
+//            }
+//        }
+//        
+//        // add the news with keyword "nature" in the titile into news arrayList
+//        for (int i = 0; i < titleList.size(); i++) {
+//            news news = new news(titleList.get(i), urlList.get(i), dateList.get(i));
+//            newsArray.add(news);
+//        }
+//        
+//        // sort the ArrayList of News according to their specificDate, from the latest to the oldest news
+//        Collections.sort(newsArray, Collections.reverseOrder());
+//        
+//        return newsArray;
+//    }
+//    
+//    
+//    // create a method to read file as list
+//    public static ArrayList<String> readFileInArrayList(String filepath) {
+//        List<String> fileList = new ArrayList<>();
+//        try {
+//            fileList = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return (ArrayList<String>)fileList;
+//    }
+//    
+//    
+//    // create a method to create titleList/urlList/dateList based on their number of line
+//    public static ArrayList<String> getArrayList(ArrayList<String> originalArrayList, int multipleOfLine) {
+//        ArrayList<String> newArray = new ArrayList<>();
+//        for (int i = 0; i < originalArrayList.size(); i++) {
+//            if ((i-multipleOfLine)%3 == 0) {
+//                newArray.add(originalArrayList.get(i));
+//            }
+//        }
+//        return newArray;
+//    }
 }
