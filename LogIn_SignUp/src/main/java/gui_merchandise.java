46147d29fef1078fb30e8a0e5f533cb99a80d2be
GuakLaLa <149032640+GuakLaLa@users.noreply.github.com>
2023-12-30 23:11:46 +0800

    merchandise

--- LogIn_SignUp/src/main/java/gui_merchandise.java
+++ LogIn_SignUp/src/main/java/gui_merchandise.java
@@ -643,10 +643,6 @@ public class gui_merchandise extends javax.swing.JFrame {
                     JOptionPane.showMessageDialog(null, "Invalid quantity format. Please enter a valid number.");
                     // Print the exception details for debugging purposes
                 }
-                
-                text_obj2Quantity.setText("");
-                text_obj2Address.setText("");
-                
             } else {
                 // Handle the case where the quantity is empty
                 JOptionPane.showMessageDialog(null, "Textfield cannot be empty. Please enter a valid number and address.");
@@ -700,17 +696,12 @@ public class gui_merchandise extends javax.swing.JFrame {
                     }else{
                         JOptionPane.showMessageDialog(null, "You do not have enough point.");
                     }
-                    
                 
                 } catch (NumberFormatException e) {
                     // Handle the case where parsing fails
                     JOptionPane.showMessageDialog(null, "Invalid quantity format. Please enter a valid number.");
                     // Print the exception details for debugging purposes
                 }
-                
-                text_obj1Quantity.setText("");
-                text_obj1Address.setText("");
-                
             }else {
                 // Handle the case where the quantity is empty
                 JOptionPane.showMessageDialog(null, "Textfield cannot be empty. Please enter a valid number and address.");
@@ -760,10 +751,6 @@ public class gui_merchandise extends javax.swing.JFrame {
                     JOptionPane.showMessageDialog(null, "Invalid quantity format. Please enter a valid number.");
                     // Print the exception details for debugging purposes
                 }
-                
-                text_obj3Quantity.setText("");
-                text_obj3Address.setText("");
-                
             } else {
                 // Handle the case where the quantity is empty
                 JOptionPane.showMessageDialog(null, "Textfield cannot be empty. Please enter a valid number and address.");
@@ -909,40 +896,6 @@ public class gui_merchandise extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(gui_donation.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(gui_donation.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(gui_donation.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(gui_donation.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new gui_merchandise().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_Quiz;
